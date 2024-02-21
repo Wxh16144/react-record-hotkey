90bfa51b2490aa2c010aaa48a4be4eca413ff0e3
wuxh <wxh1220@gmail.com>
2024-02-21 22:13:19 +0800

    chore: add test

--- packages/react-use-record-hotkey/src/__tests__/index.spec.ts
+++ packages/react-use-record-hotkey/src/__tests__/index.spec.ts
@@ -1,3 +1,8 @@
-it('should work', () => {
-  expect(true).toBeTruthy();
+import useRecordHotkeyDefault, { useRecordHotkey } from 'react-use-record-hotkey';
+
+describe('useRecordHotkey', () => {
+  it('should be a function', () => {
+    expect(useRecordHotkeyDefault).toBeInstanceOf(Function);
+    expect(useRecordHotkey).toBe(useRecordHotkeyDefault);
+  });
 });
