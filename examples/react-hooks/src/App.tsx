bdeecc521b5812cc834b385c3b51175499eac4ec
wuxh <wxh1220@gmail.com>
2024-02-23 15:03:12 +0800

    chore: update

--- examples/react-hooks/src/App.tsx
+++ examples/react-hooks/src/App.tsx
@@ -10,13 +10,15 @@ const App = () => {
     },
   });
 
+  const hotkey = Array.from(keys).join('+');
+
   return (
     <div>
-      <input ref={inputRef} autoFocus readOnly />
+      <input ref={inputRef} autoFocus readOnly value={hotkey} />
       <button onClick={start}>Start</button>
       <button onClick={stop}>Stop</button>
       <p>Recording: {isRecording ? 'Yes' : 'No'}</p>
-      <p>Hotkey: {Array.from(keys).join('+')}</p>
+      <p>Hotkey: {hotkey}</p>
     </div>
   );
 };
