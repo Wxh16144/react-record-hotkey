4f807dec5ec4aeb99fb4f8564c542cdf4a739236
红 <wxh1220@gmail.com>
2024-04-12 20:47:23 +0800

    fix: 修复禁用功能不工作错误 (#4)

--- .fatherrc.base.ts
+++ .fatherrc.base.ts
@@ -1,6 +1,10 @@
+import babelPluginImportLib2Es from '@rc-component/father-plugin/dist/babelPluginImportLib2Es';
 import { defineConfig } from 'father';
 
 export default defineConfig({
   cjs: { output: 'lib' },
-  esm: { output: 'es' },
+  esm: {
+    output: 'es',
+    extraBabelPlugins: [babelPluginImportLib2Es],
+  },
 });
