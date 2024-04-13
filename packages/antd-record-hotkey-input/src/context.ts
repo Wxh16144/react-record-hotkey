cee9d9f2c6fe80cc102cdb0ccd06777205b6819e
wuxh <wxh1220@gmail.com>
2024-04-13 11:29:25 +0800

    feat: 添加类名

--- packages/antd-record-hotkey-input/src/context.ts
+++ packages/antd-record-hotkey-input/src/context.ts
@@ -1,2 +1,8 @@
+import { ConfigProvider } from 'antd';
+const { ConfigContext } = ConfigProvider;
+
 export { default as DisabledContext } from 'antd/lib/config-provider/DisabledContext';
 export { default as SizeContext } from 'antd/lib/config-provider/SizeContext';
+
+// export { ConfigContext} from 'antd/lib/config-provider/context';
+export { ConfigContext };
