39f8f4aca499ad9eedf9808647a85e47577915db
wuxh <wxh1220@gmail.com>
2024-04-14 09:50:58 +0800

    feat: 操作图标按钮添加默认行为
    size 不等于 large 时添加最高高度，避免闪烁

--- packages/antd-record-hotkey-input/src/ActionIcon.tsx
+++ packages/antd-record-hotkey-input/src/ActionIcon.tsx
@@ -3,7 +3,7 @@ import { Button, Tooltip } from 'antd';
 import cx from 'clsx';
 import type { CSSProperties } from 'react';
 import { useContext } from 'react';
-import { ConfigContext, DisabledContext } from './context';
+import { ConfigContext, DisabledContext, SizeContext } from './context';
 
 export interface ActionIconProps extends Omit<ButtonProps, 'title'> {
   title?: TooltipProps['title'];
@@ -13,22 +13,36 @@ export interface ActionIconProps extends Omit<ButtonProps, 'title'> {
 }
 
 const ActionIcon = (props: ActionIconProps) => {
-  const { placement, title, icon, cursor, disabled, style, className, ...restProps } = props;
+  const { placement, title, icon, cursor, disabled, style, className, size, ...restProps } = props;
 
   const ctxDisabled = useContext(DisabledContext);
   const mergedDisabled = disabled ?? ctxDisabled;
+
+  const ctxSize = useContext(SizeContext);
+  const mergedSize = size ?? ctxSize;
 
   const { getPrefixCls } = useContext(ConfigContext);
 
   const prefixCls = getPrefixCls('action-icon');
   const cls = cx(className, prefixCls);
 
+  const btnStyle: React.CSSProperties = {
+    /**
+     * 这里期望的是开发者传递给 suffix 时不出现抖动问题。
+     * 当然开发者可以通过 style 覆盖这个样式。
+     */
+    ...(mergedSize !== 'large' ? { maxHeight: 22 } : {}),
+    ...style,
+    cursor,
+  };
+
   let iconNode = (
     <Button
       icon={icon}
-      style={{ ...style, cursor }}
+      style={btnStyle}
       disabled={mergedDisabled}
       className={cls}
+      size={mergedSize}
       {...restProps}
     />
   );
